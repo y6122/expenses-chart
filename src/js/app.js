c01df4f82ff7d9a91a68591ebd9ed2524f61680f
yassein ali <yasen6122@yahoo.com>
2023-10-17 04:20:31 +0400

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,75 @@
+import Chart from "chart.js/auto";
+
+const chartBox = document.getElementById("chart-bar");
+
+async function feachJson() {
+  const response = await fetch("./src/js/data.json");
+  const data = await response.json();
+
+  function colorOfBar(){
+    const maxAmount= Math.max(...data.map((item)=>item.amount))
+    return data.map(item=>{
+      return item.amount==maxAmount?'hsl(186, 34%, 60%)':'hsl(10, 79%, 65%)'
+    })
+    
+      
+      
+   }
+  
+  
+  return new Chart(chartBox, {
+    type: "bar",
+    data: {
+      labels: data.map((item) => item.day),
+      datasets: [
+        {
+          backgroundColor: colorOfBar(),
+          borderRadius: 5,
+          borderSkipped: "middle",
+          label: false,
+          data: data.map((item) => item.amount),
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        tooltip: {
+          yAlign: "bottom",
+          displayColors: false,
+          caretSize: 0,
+          caretPadding: 5,
+
+          callbacks: {
+            title: () => "",
+            label: function (context) {
+              let label = context.parsed.y;
+              return "$" + label;
+            },
+          },
+        },
+        legend: {
+          display: false,
+        },
+      },
+      scales: {
+        y: {
+          display: false,
+        },
+
+        x: {
+          grid: {
+            display: false,
+            offset: false,
+          },
+        },
+      },
+    },
+  });
+
+  
+}
+
+feachJson();
+
+
+
